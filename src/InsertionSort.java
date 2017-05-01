5a7b32669bb6b5351e2607546e626fa677c1836e
Garima Chandra <garich01@noa.nintendo.com>
2017-05-01 16:22:27 -0700

    added comments

---
+++ src/InsertionSort.java
@@ -0,0 +1,13 @@
+
+public class InsertionSort {
+
+    public static boolean insertionSort(){
+
+        return false;
+        //in insertion sort first take the min element from the list.
+        //once that is achieved, from the remaining sub array take out the minimum
+        //
+    }
+
+
+}
